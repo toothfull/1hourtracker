f60746db31007ae7e9a47a991a872f3e459a099b
Toothfull <60007976+Toothfull@users.noreply.github.com>
2023-01-05 17:49:28 +0000

    Added location button and fix several timing bugs

--- browser/javascript/main.js
+++ browser/javascript/main.js
@@ -4,6 +4,8 @@ let newPos
 let map
 let userLine
 let username
+let distance = 1;
+let hasStarted = false;
 
 //Function for initialising the map
 function initMap() {
@@ -25,9 +27,9 @@ function initMap() {
 
 	// Adds user line
 	line = new google.maps.Polyline({
-		strokeColor: "#000000",
+		strokeColor: randomColor(),
 		strokeOpacity: 1.0,
-		strokeWeight: 3,
+		strokeWeight: 6,
  	});
 	line.setMap(map);
 
@@ -45,16 +47,17 @@ function initMap() {
 		infoWindow.close();
 	});
 
-}	
+}
+
+//random hex color generator
+function randomColor() {
+	return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 
 function liveMarker(lat, long) {
 	newPos = new google.maps.LatLng(lat, long)
 	marker.setPosition( newPos );
-	map.panTo( newPos );
 	map.setZoom(18)
-
-	
-	
 }
 
 function updateLine (lat, long) {
@@ -74,7 +77,7 @@ function countDown() {
     var now = new Date().getTime();
     
     // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+    distance = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -87,6 +90,7 @@ function countDown() {
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("stopwatch").innerHTML = "EXPIRED";
+		hasStarted = false;
     }
 }, 1000);
 }
@@ -94,10 +98,13 @@ function countDown() {
 function getLocation() {
 	if (navigator.geolocation) {
 		(navigator.geolocation.getCurrentPosition(function(position){
-			console.log(position.coords.latitude, position.coords.longitude);
-			liveMarker(position.coords.latitude, position.coords.longitude);
-			updateLine(position.coords.latitude, position.coords.longitude);
-			setTimeout(getLocation, 1000);
+			map.panTo( new google.maps.LatLng(position.coords.latitude, position.coords.longitude) );
+			//Timer reaches 0
+			if (distance > 0) {
+				liveMarker(position.coords.latitude, position.coords.longitude);
+				updateLine(position.coords.latitude, position.coords.longitude);
+				setTimeout(getLocation, 60000);
+			}
 		})); 
 	} else {
 		console.log("Geolocation is not supported by this browser.");
@@ -105,23 +112,38 @@ function getLocation() {
 }
 
 $('#submit').click(() => {
+	if (hasStarted == false) {
+		hasStarted = true;
 
-	username = validation($('#username').val());
+		username = validation($('#username').val());
 
-	if (username == false) {
-		alert('Username is not valid');
-		$('#username').val('');
+		if (username == false) {
+			alert('Username is not valid');
+			$('#username').val('');
+		}
+		else {
+			$.post( "http://localhost:1000/username", { username: username }, function (data){
+				alert(data);
+			});
+			$('#username').val('');
+			countDown();
+			initMap();
+			getLocation();
+		}
+	} 
+	else {
+		alert('You have already a session');
+	}
+
+});
+
+$('#myLocation').click(() => {
+	if (hasStarted == false) {
+		alert('You have to start a session');
 	}
 	else {
-		$.post( "http://localhost:1000/username", { username: username }, function (data){
-			alert(data);
-		});
-		$('#username').val('');
-		initMap();
+		map.panTo( newPos );
 	}
-
-	getLocation();
-
 });
 
 window.onload = function() {
